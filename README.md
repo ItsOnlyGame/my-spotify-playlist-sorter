17e5b3f183e24479f0d848fad714fc28e9a2e46f
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2021-12-03 18:52:29 +0200

    Final release

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # Spotify playlist sorter
-This script sorts your spotify playlist.
+This script sorts your spotify playlist.  
+
 Sorting of the playlist follows these rules:
 1. Playlist gets sorted in artists, they are sorted with the date added to the playlist
 2. Every artists song will get sorted with it's albums based on release date
 3. Album tracks will be sorted on the order that they should be if they were in the actual album. 
-
-# Disclaimer
-This is script doesn't have every functionality added yet
-For example, the rules only abide by the first rule 
-
-# Copyright
--- main.py
+++ main.py
@@ -1,62 +1,113 @@
 import spotipy
 import os
 from dotenv import load_dotenv
-import list_utils
+import time
 
 def getPlaylistItems():
     offset = 0
     items = []
-    artist_order = []
 
     while True:
         results = sp.playlist_items(playlist_url, limit=100, offset=offset)
 
         for item in results['items']:
-            artist_order.append(item['track']['artists'][0]['name'])
             items.append(item)
 
         offset += 100
         if len(results['items']) != 100:
             break
 
-    iterated_artists = []
-    for item in artist_order[:]:
-        if item in iterated_artists:
-            artist_order.remove(item)
-        else:
-            iterated_artists.append(item)
-
-    return { 'tracks': items, 'sort_order': iterated_artists }
+    return items
 
 
-def sortPlaylistList(data):
-    grouped_artists = []
+def sort_playlist():
+    playlist_tracks = getPlaylistItems()
 
-    for order in data['sort_order']:
-        artist_group = []
+    data = {};
+    for item in playlist_tracks:
+        album_name = item['track']['album']['name']
+        artist_name = item['track']['artists'][0]['name']
 
-        for track_item in data['tracks']:
-            if (order == track_item['track']['artists'][0]['name']):
-                artist_group.append(track_item)
-                pass
+        if not artist_name in data.keys():
+            data[artist_name] = {};
+        if not album_name in data[artist_name].keys():
+            data[artist_name][album_name] = [];
+        data[artist_name][album_name].append(item)
 
-        grouped_artists.extend(artist_group);
+    for artists in data.keys():
+        for album in data[artists].keys():
+            album_tracks = sp.album_tracks(data[artists][album][0]['track']['album']['id'])['items']
+            
+            new_track_array = []
+            for sp_track in album_tracks:
+                for track in data[artists][album]:
+                    if sp_track['id'] == track['track']['id']:
+                        new_track_array.append(track)
 
-    return grouped_artists
+            data[artists][album] = new_track_array
 
+    
+    sorted_playlist = []
+    for artists in data.keys():
+        album_list = list(data[artists])
+
+        for i in range(0, len(album_list) - 1):
+
+            first_key = album_list[i];
+            second_key = album_list[i + 1];
+
+            first_album = sp.album(data[artists][first_key][0]['track']['album']['id'])
+            second_album = sp.album(data[artists][second_key][0]['track']['album']['id'])
+
+            first_release_date = time.strptime(first_album['release_date'], "%Y-%m-%d")
+            second_release_date = time.strptime(second_album['release_date'], "%Y-%m-%d")
+
+            print(first_release_date)
+            print(second_release_date)
+
+            if (first_release_date > second_release_date): 
+                album_list.insert(i+1, album_list.pop(i))
+
+            print([album for album in album_list])
+        
+        for album in album_list:
+            for track in data[artists][album]:
+                sorted_playlist.append(track)
+
+    return sorted_playlist
+
+
+# https://open.spotify.com/playlist/60xD9HqNbgojJpeCLiUNJX?si=208e5d5e8c39474e
 
 def execute():
-    playlist = getPlaylistItems()
-    sorted_list = sortPlaylistList(playlist)
+    sorted_list = sort_playlist()
+    snapshot = None
 
-    for i, t1 in enumerate(sorted_list):
-        for j, t2 in enumerate(playlist['tracks']):
-            if i == j:
-                continue
+    print("Sorting playlist. This might take a while depending on the size of your playlist")
+    iteration = 1
+    while True:
+        playlist = getPlaylistItems()
+        sorted = True
 
-            if t1['track']['uri'] == t2['track']['uri']:
-                sp.playlist_reorder_items(playlist_url, range_start=j, insert_before=i)
-                list_utils.move_list_item(playlist['tracks'], j, i)
+        print("Running the "+iteration+" iteration.")
+
+        for j in range(0, len(playlist)):
+            if playlist[j]['track']['uri'] != sorted_list[j]['track']['uri']:
+                sorted = False
+                for i, t1 in enumerate(sorted_list):
+                    if t1['track']['uri'] == playlist[j]['track']['uri']:
+                        if snapshot == None:
+                            response = sp.playlist_reorder_items(playlist_url, range_start=j, insert_before=i+1)
+                            snapshot = response['snapshot_id']
+                        else:
+                            response = sp.playlist_reorder_items(playlist_url, range_start=j, insert_before=i+1, snapshot_id=snapshot)
+                            snapshot = response['snapshot_id']
+
+                        playlist.insert(i, playlist.pop(j))
+                        break
+        iteration += 1
+        if sorted: 
+            break
                 
 
 if __name__ == '__main__':
@@ -77,5 +128,7 @@ if __name__ == '__main__':
     playlist_url = input('Give playlist url: \n')
 
     execute()
+    #sp.playlist_reorder_items(playlist_url, range_start=0, insert_before=3)
+
     print('Finished!')   
  
