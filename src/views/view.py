9acce92f1c50ca23cd684e7e36d1522131ac2718
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-28 14:45:11 +0200

    Clear terminal when get_input is called

--- src/views/view.py
+++ src/views/view.py
@@ -2,12 +2,12 @@ from os import system, name
 from art import tprint
 
 class View:
-    def __init__(self, title) -> None:        
-        self.clear()
-        tprint("Spotify Playlist Sorter")
-
-    def get_input(self):
+    def __init__(self) -> None:        
         pass
+
+    def get_input(self, text="Spotify Playlist Sorter"):
+        self.clear()
+        tprint(text)
 
     def clear(self):
         # for windows
